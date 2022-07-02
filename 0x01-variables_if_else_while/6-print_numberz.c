5ff753025bf367d02090777624bb8471830c7e14
Joyful Daphnes <79005254+daphnesaj@users.noreply.github.com>
2022-07-02 00:05:57 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,14 @@
 /* more headers go here */
 
 /**
- * main: - Entry point
+ * main - Entry point
+/** Description: prints all single digit numbers of base 10 starting from 0*/
  * Return: Always 0 (Success)
  */
 
 
 int main(void)
-/** Description: prints all single digit numbers of base 10 starting from 0*/
+
 {
 	int i;
 
