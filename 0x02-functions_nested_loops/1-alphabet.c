fa61d9f4bf458440544d928e8aac4c18da649588
Joyful Daphnes <79005254+daphnesaj@users.noreply.github.com>
2022-07-05 20:37:13 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@
 
 void print_alphabet(void)
 {
-  char alpha[]="abcdefghijklmnopqrstuvwxyz";
+  char alpha[] = "abcdefghijklmnopqrstuvwxyz";
   int i;
   for (i = 0; i < 26; i++) 
   _putchar(alpha[i]);
