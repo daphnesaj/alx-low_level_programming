d86d1f3afca8e901dfbea58d718e67b185e6fe91
daphnesaj <daphnesaj@gmail.com>
2022-07-05 14:11:07 +0000

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,11 @@
 
 int main (void)
 {
-  char word[10] = "_putchar"
+  char word[10] = "_putchar";
   int  i;
 
   for ( i = 0; i < 8; i++) {
-    _putchar(word[i])
+    _putchar(word[i]);
   }
   return (0) ;
 }
